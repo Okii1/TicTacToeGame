ec3bb4c59abe0e29ea5a03b1fff7d5c9be1da0a7
Orhan <93547088+Okii1@users.noreply.github.com>
2024-07-29 14:44:51 +0200

    tictacGame

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,45 @@
+
+const initalGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({onSelectSquare, turns}) {
+    let gameBoard = initalGameBoard;
+
+    for (const turn of turns){
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+
+/*  const [gameBoard, setGameBoard] = useState(initalGameBoard);
+  
+  function handleSelectSquare(rowIndex, colIndex){
+    setGameBoard((prevGameBoard) => {
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+        updatedBoard[rowIndex][colIndex] = activePlayerSimbol;
+        return updatedBoard;
+    } );
+
+    onSelectSquare();
+  }
+  */
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol, colIndex) => (<li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                disabled={playerSymbol !==null}>
+                    {playerSymbol}</button> 
+                </li>))}
+            </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
